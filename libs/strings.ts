96bde52c756b3922d2b99be336435db8e0887511
Norton Glitz <norton.glitz@gmail.com>
2024-12-30 23:52:55 -0300

    feat: format lat and lon function

--- libs/strings.ts
+++ libs/strings.ts
@@ -5,3 +5,11 @@ export const countryCodeToFlagEmoji = (countryCode: string) => {
     .map(char => String.fromCodePoint(0x1f1e6 + char.charCodeAt(0) - 65))
     .join("")
 }
+
+export const formatLatAndLon = (latOrLon: number) => {
+  return (
+    new Intl.NumberFormat("pt-BR", {
+      minimumFractionDigits: 4
+    }).format(latOrLon) + "°"
+  )
+}
