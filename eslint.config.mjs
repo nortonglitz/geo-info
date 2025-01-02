428a9491255c2e2ae6834cfbe7404129e9a72c7c
Norton Glitz <norton.glitz@gmail.com>
2025-01-02 15:58:29 -0300

    fix: eslint working properly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
+import eslintPluginPrettier from "eslint-plugin-prettier"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,13 +11,18 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
+  {
+    ignores: [".next", "node_modules"]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "prettier/prettier": "warn"
     },
-    plugins: ["prettier"]
+    plugins: {
+      prettier: eslintPluginPrettier
+    }
   }
 ]
 
