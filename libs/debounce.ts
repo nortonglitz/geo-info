98910cfd6cbca0e0ac78956f080785fb58894f47
Norton Glitz <norton.glitz@gmail.com>
2025-01-03 19:43:58 -0300

    fix: use any on debounce params instead of unknown

--- libs/debounce.ts
+++ libs/debounce.ts
@@ -1,4 +1,8 @@
-export const debounce = <T extends (...args: unknown[]) => unknown>(fc: T, timer: number) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const debounce = <T extends (...args: any[]) => unknown>(
+  fc: T,
+  timer: number
+): ((...args: Parameters<T>) => void) => {
   let timeout: NodeJS.Timeout | null = null
 
   return (...args: Parameters<T>): void => {
