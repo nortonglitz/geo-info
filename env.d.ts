4cfceed2dec0d1fd886af70d12120f3ccd308f77
Norton Glitz <norton.glitz@gmail.com>
2025-01-03 19:08:29 -0300

    chore: add timezonedb on process types

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_NOMINATIM_API_BASE_URL: string
     NEXT_PUBLIC_OPENMETEO_API_BASE_URL: string
+    TIMEZONEDB_API_KEY: string
+    TIMEZONEDB_API_BASE_URL: string
   }
 }
