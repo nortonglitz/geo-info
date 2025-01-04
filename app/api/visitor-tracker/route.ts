9ca46632e2fd8d6be061ac4372d51f32358dba0d
Norton Glitz <norton.glitz@gmail.com>
2025-01-04 00:46:18 -0300

    feat: create visitor-tracker route

---
+++ app/api/visitor-tracker/route.ts
@@ -0,0 +1,42 @@
+import { getUserLocationByIp } from "@/services/ipinfo"
+import { NextRequest, NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const forwardedFor = req.headers.get("x-forwarded-for")
+    let ip =
+      typeof forwardedFor === "string"
+        ? forwardedFor.split(",")[0].trim() // Obtém o primeiro IP da lista
+        : "127.0.0.1" // Alternativa para o IP local
+
+    if (ip === "::1") {
+      ip = "127.0.0.1"
+    }
+
+    const visitorExists = await prisma.visitor.findUnique({
+      where: { ip }
+    })
+
+    if (visitorExists) {
+      await prisma.visitor.update({
+        where: { ip },
+        data: {
+          visitCount: { increment: 1 }
+        }
+      })
+    } else {
+      const { city, region, country } = await getUserLocationByIp(ip)
+      await prisma.visitor.create({
+        data: { ip, city, region, country }
+      })
+    }
+
+    return NextResponse.json({ message: "Visitante processado com sucesso." }, { status: 200 })
+  } catch (err) {
+    console.error(err)
+    return NextResponse.json({ error: "Não foi possível processar visitante." }, { status: 500 })
+  }
+}
